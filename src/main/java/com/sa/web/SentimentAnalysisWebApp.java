5ed06ee2d220d79220e0d37a9ba986dcc5992e79
snowdevops <52936916+snowdevops@users.noreply.github.com>
2019-08-17 22:50:17 -0700

    Update SentimentAnalysisWebApp.java

--- src/main/java/com/sa/web/SentimentAnalysisWebApp.java
+++ src/main/java/com/sa/web/SentimentAnalysisWebApp.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SentimentAnalysisWebApp {
 
-	//main
+	//main class
 	public static void main(String[] args) {
 		System.out.println("Starting SentimentAnalysisWebApp...");
 		System.out.println("hotfix");
